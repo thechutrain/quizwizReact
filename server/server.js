803a7dbc84dcc2cb1f6ca7734997eca3f57bb54a
thechutrain <chu.chuyalan@gmail.com>
2017-04-07 15:47:19 -0400

    making it an absolute path yo

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     // res.send('hey there')
     res.type('.html')
-    res.sendFile('../client/build/index.html')
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 }
 // Start server ---------------------------------- /
