0bc7600aeffc569571b82ec870c673cc723913f2
thechutrain <chu.chuyalan@gmail.com>
2017-04-09 10:50:43 -0400

    added bootstrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import axios from 'axios'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import NavBar from './NavBar/NavBar'
 
 const App = React.createClass({
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,20 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Button, Nav, Navbar, NavItem } from 'react-bootstrap'
 import './NavBar.css'
 
-const NavBar = React.createClass({
+const Navigation = React.createClass({
   render () {
     return (
       <Router>
         <div className="navbar-container">
-        <h1>Welcome!!</h1>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a className='brand-name' href='/'>QuizWiz</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+              <NavItem href="#">Home</NavItem>
+              <NavItem href="/about">About</NavItem>
+            </Nav>
+          </Navbar>
 
+        <Button> Click Me! </Button>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/about" component={About}></Route>
+
         </div>
       </Router>
     )
@@ -34,4 +43,4 @@ const About = () => (
   </div>
 )
 
-export default NavBar
+export default Navigation
